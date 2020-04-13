ec3c25bba5e79f0ab6b491a28633aae6c2c89d4c
Brittiany Juravic <britjuravic@Brittianys-MacBook-Pro.local>
2020-04-13 12:28:46 -0700

    Users Class methods WIP

--- models/user.js
+++ models/user.js
@@ -1,3 +1,7 @@
+const { BCRYPT_WORK_FACTOR } = require("../config.js");
+const db = require("../db");
+const ExpressError = require("../expressError");
+
 /** User class for message.ly */
 
 
@@ -10,15 +14,45 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    const hashedPassword = await bcrypt.hash(password,
+                                              BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING username`, [username, hashedPassword, first_name, last_name, phone]);
+
+    if (result.rows.length === 0) {
+      throw new ExpressError("Invalid Inputs");
+    }
+
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+       FROM users
+       WHERE username = $1`, [username]);
+
+    const user = result.rows[0];
+
+    if (user) {
+      if (await bcrypt.compare(password, user.password) === true) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+
+   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
